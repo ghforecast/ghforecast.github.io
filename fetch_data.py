f9238bce231b9714229cc6ab05593438b37e44a2
ghforecast <128191306+ghforecast@users.noreply.github.com>
2023-03-17 15:35:47 -0700

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -11,7 +11,7 @@ def cache_data(data, cache_file):
 
 if __name__ == '__main__':
     API_URL = 'https://api.avalanche.org/v2/public/product?type=forecast&center_id=SAC&zone_id=77'
-    CACHE_FILE = 'data/data.json'
+    CACHE_FILE = 'docs/data.json'
 
     data = fetch_data(API_URL)
     cache_data(data, CACHE_FILE)
